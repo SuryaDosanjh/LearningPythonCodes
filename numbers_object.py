f9fac2482eb28e0debe475529aac2fd8d7d1a649
surya <surya.dosanjh@psychx86.com>
2026-08-17 15:44:19 +0530

    feat<numbers>: number object codes

---
+++ numbers_object.py
@@ -0,0 +1,26 @@
+a = 3           # a variable declaration
+b = 4
+
+# numeric operations
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)          # modulus - returns a division's remainder
+print(a**b)
+print(a//b)          #Floor division- truncates fractional remainders down to their floor
+
+#comparison operators
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a == b)
+print(a != b)
+
+# truncation and floor methods of math module
+import math
+print(math.floor(2.5))
+print(math.trunc(2.5))
+print(math.floor(-2.5))
+print(math.trunc(-2.5))
